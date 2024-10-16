bfab08e05ff5ac4f18f4310e45b055f4238b41a5
John aaaaDoe <aaaaae@example.com>
2024-10-16 10:48:49 +0000

    commit

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,19 @@
+import { browser } from '$app/environment'; // Import to check if running on the client
+import { goto } from '$app/navigation';
+
 export const load = async ({ url: { pathname } }) => {
-	return { pathname };
-	
+  // Define paths you want to redirect from
+  const redirects = {
+    '/beermoney-sites': '/money-sites',
+    '/money-making': '/money-sites',
+  };
+
+  // Only perform the redirection on the client-side
+  if (browser && redirects[pathname]) {
+    goto(redirects[pathname], { replaceState: true });
+  }
+
+  return { pathname };
 };
 
-export const prerender = true
+export const prerender = true;
